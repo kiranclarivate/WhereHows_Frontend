616b26b7095debaac0ad6791489d87fdd2ee823d
richardxin <richardxin168@yahoo.com>
2018-04-25 14:03:07 -0700

    add debugging

--- wherehows-frontend/app/controllers/api/v1/Search.java
+++ wherehows-frontend/app/controllers/api/v1/Search.java
@@ -133,8 +133,13 @@ public class Search extends Controller {
     String source = request().getQueryString("source");
     String pageStr = request().getQueryString("page");
     String dsCategory = request().getQueryString("ds_category");
+    String dummy = request().getQueryString("dummy");
 
+    Logger.info("====== keyword ======" + keyword);
+    Logger.info("====== category ======" + category);
+    Logger.info("====== pageStr ======" + pageStr);
     Logger.info("====== dsCategory ======" + dsCategory);
+    Logger.info("====== dummy ======" + dummy);
 
     if (isBlank(pageStr)) {
       page = 1;
