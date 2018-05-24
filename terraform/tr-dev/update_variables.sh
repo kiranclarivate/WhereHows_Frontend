37a6d54b6039bd05a976f27d269b7c0238cdc094
richardxin <richardxin168@yahoo.com>
2018-05-24 09:48:53 -0700

    rollback

--- terraform/tr-dev/update_variables.sh
+++
@@ -1,2 +0,0 @@
-sed -i "s|$INSTANCE_TYPE|${INSTANCE_TYPE}|g" variables.tf.template
-sed -i "s|$INSTANCE_COUNT|${INSTANCE_COUNT}|g" variables.tf.template
--- terraform/tr-dev/variables.tf
+++ terraform/tr-dev/variables.tf
@@ -3,14 +3,14 @@ variable "instance_groups" {
     {
       name           = "MasterInstanceGroup"
       instance_role  = "MASTER"
-      instance_type  = "$INSTANCE_TYPE"
+      instance_type  = "#INSTANCE_TYPE"
       instance_count = 1
     },
     {
       name           = "CoreInstanceGroup"
       instance_role  = "CORE"
       instance_type  = "$INSTANCE_TYPE"
-      instance_count = $INSTANCE_COUNT
+      instance_count = #INSTANCE_COUNT
       bid_price      = "0.30"
     },
   ]
