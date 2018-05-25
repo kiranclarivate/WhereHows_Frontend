cd02526d36ca5b5ef8f0622f90c98b228f191f86
richardxin <richardxin168@yahoo.com>
2018-05-25 15:57:20 -0700

    code clean up

--- terraform/cc-dev/get-latest-terraform.sh
+++ terraform/cc-dev/get-latest-terraform.sh
@@ -3,8 +3,8 @@
 LATEST_RELEASE=$(curl https://api.github.com/repos/hashicorp/terraform/releases/latest | jq --raw-output '.tag_name' | cut -c 2-)
 if [[ ! -e ${LATEST_RELEASE} ]]; then
    echo "Installing Terraform ${LATEST_RELEASE}..."
-   rm terraform-*
-   rm terraform
+   # rm terraform-*
+   # rm terraform
    wget https://releases.hashicorp.com/terraform/${LATEST_RELEASE}/terraform_${LATEST_RELEASE}_linux_amd64.zip
    unzip terraform_${LATEST_RELEASE}_linux_amd64.zip
    rm terraform_${LATEST_RELEASE}_linux_amd64.zip
