78e7747e5511742e4fead187f6253804aa9790a2
richardxin <richardxin168@yahoo.com>
2018-06-01 16:14:42 -0700

    false

--- terraform/cc-dev/main.tf
+++ terraform/cc-dev/main.tf
@@ -8,7 +8,7 @@ resource "aws_emr_cluster" "cluster" {
   applications  = #APPLICATIONS
 
   termination_protection = false
-  keep_job_flow_alive_when_no_steps = true
+  keep_job_flow_alive_when_no_steps = false
 
   ec2_attributes {
     subnet_id                         = "subnet-a93c6ede"
